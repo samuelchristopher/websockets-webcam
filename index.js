75d2adf741026819e1779572121778b18fcf4410
Samuel Christopher <thesamchris@gmail.com>
2017-09-01 23:21:49 -0700

    Added views and dev environment

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from 'express'
 const app = express()
-app.get('/', (req, res) => res.send('Hello Web Cam bro!'))
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => res.render('controller', {message: 'Howdy friend'}))
+app.get('/camera', (req, res) => res.render('camera', { message: 'Hi cam friend' }))
+
 app.listen(8000, () => console.log('app is running on localhost:8000'))
